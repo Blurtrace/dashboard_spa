901bfcc755603aee6bc9a2e47d55ebc27c4bf008
elianis cervantes <elianiscervantessanchez@gmail.com>
2026-05-29 07:01:54 -0500

    mergin conflict solved

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -3,4 +3,33 @@ export function homeController(){
 
     const userInfo = document.getElementById("spanUserName");
     userInfo.innerText = user.full_name;
-}
+}import Swal from "sweetalert2";
+// ===================================
+// 🏠 HOME 
+// ===================================
+
+export function homeController(user) {
+    homeListeners(user);
+}                          
+
+function homeListeners(user) {  
+    const hamburger = document.getElementById("menu-btn");
+
+    hamburger.addEventListener("click", () => {
+
+        const sidebar = document.getElementById("sidebar");
+
+        sidebar.classList.toggle("hidden");
+
+    });
+
+    const cardContainer = document.getElementById("homeCard");
+
+    user.forEach((user) => {
+        
+
+    })
+
+
+
+}                          
